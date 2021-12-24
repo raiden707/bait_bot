9a582f608570b9def623a69af569992e2ab61323
raiden707 <rai7git@gmail.com>
2021-12-24 13:20:15 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+token = os.environ.get("TOKEN")
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Standing by...")
+
+@client.event
+async def on_message(message):
+    if message.author.id == 191373844659830785:
+        await message.channel.send("https://media1.giphy.com/media/IDGNYvFLkJKLK/giphy.gif?cid=ecf05e47u8uggyhua9oidbpct0y8fd4twggl69m7if69ua8p&rid=giphy.gif&ct=g")
+
+client.run(token)
