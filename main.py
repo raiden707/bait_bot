f2247bd43b0b6cadffeb576a5f684e9ca74a000d
raiden707 <68893087+raiden707@users.noreply.github.com>
2021-12-24 13:31:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author.id == 191373844659830785:
+    if message.author.id == :
         await message.channel.send("https://media1.giphy.com/media/IDGNYvFLkJKLK/giphy.gif?cid=ecf05e47u8uggyhua9oidbpct0y8fd4twggl69m7if69ua8p&rid=giphy.gif&ct=g")
 
 client.run(token)
